ee746eab001382c41cfa471d1f38744757e95374
Oleksandr Stepanenko <stepanenko.alvl@gmail.com>
2019-12-04 16:13:31 +0200

    fixes

--- calculator/my_math.py
+++ calculator/my_math.py
@@ -1,11 +1,24 @@
+def check(v1, v2):
+	return isinstance(v1, (int, float)) and isinstance(v2, (int, float))
+
 def add(v1, v2):
+	if not check(v1, v2):
+		return "Incorrect types"
 	return v1+v2
 
 def sub(v1, v2):
+	if not check(v1, v2):
+		return "Incorrect types"
 	return v1-v2
 
 def mul(v1, v2):
+	if not check(v1, v2):
+		return "Incorrect types"
 	return v1*v2
 
 def div(v1, v2):
+	if not check(v1, v2):
+		return "Incorrect types"
+	if v2 == 0:
+		return "Faild v2 is 0!"
 	return v1/v2
